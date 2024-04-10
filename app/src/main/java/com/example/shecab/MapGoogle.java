82bd3b65cc6c7235c3df9e0a42b63f2dff6ff107
Entroshock <alextsang@live.ie>
2024-04-10 11:09:30 +0100

    New branch for more detailed map feature implementation

--- app/src/main/java/com/example/shecab/MapGoogle.java
+++ app/src/main/java/com/example/shecab/MapGoogle.java
@@ -8,6 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MapGoogle extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap gMap;
@@ -22,6 +26,18 @@ public class MapGoogle extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        // Set the map coordinates to Kyoto Japan.
+        LatLng kyoto = new LatLng(35.00116, 135.7681);
+        // Set the map type to Hybrid.
 
+        // Add a marker on the map coordinates.
+        googleMap.addMarker(new MarkerOptions()
+                .position(kyoto)
+                .title("Kyoto"));
+        // Move the camera to the map coordinates and zoom in closer.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(kyoto));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        // Display traffic.
+        googleMap.setTrafficEnabled(true);
     }
 }
